563623aa65b2142593fb3b3ecaf5e2d6efd7d246
oussama-el1 <elhadraoui284@example.com>
2024-02-22 19:32:08 +0100

    task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,20 +1,14 @@
 #!/usr/bin/python3
-"""
-script that return information about todos for rmployer
-"""
+"""Returns to-do list information for a given employee ID."""
 import requests
 import sys
 
-if __name__ == '__main__':
-    url = 'https://jsonplaceholder.typicode.com/'
-    employer = requests.get(url + 'users/{}'.format(sys.argv[1])).json()
-    todo_list = requests.get(url + "todos",
-                             params={"userId": sys.argv[1]}).json()
-    complited = []
-    for task in todo_list:
-        if task.get('completed') is True:
-            complited.append(task)
-    print('Employee {} is done with tasks({}/{}):'
-          .format(employer.get('name'), len(complited), len(todo_list)))
-    for task in complited:
-        print("\t {}".format(task.get('title')))
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
+    [print("\t {}".format(c)) for c in completed]
