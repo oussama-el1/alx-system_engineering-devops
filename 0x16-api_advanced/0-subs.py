5d3d51bccbbd32af393e56cea55fa31edf6fc510
oussama-el1 <elhadraoui284@gmail.com>
2024-03-07 15:46:11 +0100

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,17 +5,14 @@ import requests
 
 def number_of_subscribers(subreddit):
     "number_of_subscribers of a subreddit"
-    if subreddit is None or type(subreddit) != str or subreddit == "":
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        'User-Agent': 'alxapp/1.0 (by /u/oussamaelhadraoui777)'
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
     else:
-        url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-        headers = {
-            'User-Agent': 'alxapp/1.0 (by /u/oussamaelhadraoui777)'
-        }
-        response = requests.get(url, headers=headers)
-        if response.status_code == 200:
-            data = response.json()
-            subscribers = data['data']['subscribers']
-            return subscribers
-        else:
-            return 0
+        data = response.json()
+        subscribers = data.get('data')['subscribers']
+        return subscribers
