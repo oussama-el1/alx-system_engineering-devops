1e44cf98eb7a36a77b4ec0ec55d661331116fb1e
oussama-el1 <elhadraoui284@gmail.com>
2024-03-09 01:45:29 +0100

    task 0

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Puppet script to fix WordPress wp-settings.php file using sed replace file extention
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
